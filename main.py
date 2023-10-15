27162838ec6c90863631e3c972df6f985065a6c1
Kaumini_Perera <kauminipathirage2000@gmail.com>
2023-10-15 13:42:01 +0530

    I'm committing

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from Car import Car
 c = Car()
 print("I'm a car!")
 while True:
-    action = input("What should I do? [A]ccelerate, [B]rake, show [O]demeter, or show average [S]peed?")
-    if action not in "ABOS" or len(action) != 1:
+    action = input("What should I do? [A]ccelerate, [B]rake, show [O]demeter, show average [S]peed or [T]erminate?")
+    if action not in "ABOST" or len(action) != 1:
         print("I don't know how to do that")
         continue
     if action == "A":
@@ -15,4 +15,7 @@ while True:
         print("The car has driven {} kilometers".format(c.meter))
     elif action == "S":
         print("The car's average speed was {} kph".format(c.average_speed()))
+    else:
+        print("The car has stopped")
+        break
     c.step()
