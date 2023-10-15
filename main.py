1a6be71e37619330246879fc9d8cc52af29e93e4
BhagyaKarunarathna <147934231+BhagyaKarunarathna@users.noreply.github.com>
2023-10-15 17:41:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from Car import Car
 c = Car()
 print("I'm a car!")
 while True:
-    action = input("What should I do? [A]ccelerate,[B]rake,show [O]demeter,show average [S]peed,show [F]ly or [T]stop?")
+    action = input("What should I do? [A]ccelerate,[B]rake,show [O]demeter,show average [S]peed,show [F]ly, [W}swim, or [T]stop?")
     if action not in "ABOSFT" or len(action) != 1:
         print("I don't know how to do that")
         continue
@@ -17,6 +17,8 @@ while True:
         print("The car's average speed was {} kph".format(c.average_speed()))
     elif action == "F":
         print("The car is flying :):):)")
+    elif action == "W": 
+        print("The car is swimming :):):)")
     else:
         print("The car has stopped")
         break
